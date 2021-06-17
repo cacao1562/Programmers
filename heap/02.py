a43a3cd64c29d20883c2d133957c27dde5262dac
cacao <cacao1562@naver.com>
2021-06-18 01:27:20 +0900

    디스크 컨트롤러

---
+++ heap/02.py
@@ -0,0 +1,25 @@
+
+def solution(jobs):
+    import heapq
+    from collections import deque
+
+    tasks = deque(sorted([(x[1], x[0]) for x in jobs], key=lambda x: (x[1], x[0])))
+    q = []
+    heapq.heappush(q, tasks.popleft())
+    current_time, total_response_time = 0, 0
+    while len(q) > 0:
+        dur, arr = heapq.heappop(q)
+        current_time = max(current_time + dur, arr + dur)
+        total_response_time += current_time - arr
+      
+        while len(tasks) > 0 and tasks[0][1] <= current_time:
+            heapq.heappush(q, tasks.popleft())
+        if len(tasks) > 0 and len(q) == 0:
+            heapq.heappush(q, tasks.popleft())
+
+    return total_response_time // len(jobs) 
+
+#
+# [작업 요청 시점, 작업 소요시간] j = [[0, 3], [1, 9], [2, 6]]
+# output = 9
+#
